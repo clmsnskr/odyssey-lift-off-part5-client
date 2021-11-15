a0d40502ca140444def285fdc545fef890c5e3a8
Chris Coker <chris.coker@verys.com>
2021-11-14 21:11:47 -0500

    updated server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  // uri: 'http://localhost:4000',
+  uri: 'https://catstronauts-coker.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
